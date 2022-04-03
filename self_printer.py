eeb6c503b1b997ecaa680996fbb602a96ac946e8
Fadel Sougou <fadelsougou@gmail.com>
2022-04-03 02:00:39 +0200

    Update self_printer.py

--- self_printer.py
+++ self_printer.py
@@ -1,5 +1,5 @@
 a = "I can print myself without reading myself ^_^"
 b = "..."
-c = "Actually, not really because of the quotes and stuff, but like come on man, life is sooo so haard here in python, what did you expect?"
+c = "therefore I am!"
 W = "print(\"a = \" + a + \"b = \" + b + \"c = \" + c + \"W = \" + W + \" + W)"
 print("a = \"" + a + "\"\nb = \"" + b + "\"\nc = \"" + c + "\"\nW = \"" + W + "\"\n" + W) 
